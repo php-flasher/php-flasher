30fb4672f1c02e6d04de14b5c9a1f358800059fe
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-12-02 00:54:31 +0100

    add php-notify classes and rename namespaces

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Flasher\Toastr\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('notify_toastr');
+
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('notify_toastr');
+        }
+
+        $rootNode
+            ->children()
+                ->arrayNode('scripts')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(array(
+                        'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+                        'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js',
+                    ))
+                ->end()
+                ->arrayNode('styles')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(array(
+                        'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css',
+                    ))
+                ->end()
+                ->arrayNode('options')
+                    ->prototype('variable')->end()
+                    ->ignoreExtraKeys(false)
+                    ->defaultValue(array(
+                        'closeButton'       => true,
+                        'closeClass'        => 'toast-close-button',
+                        'closeDuration'     => 300,
+                        'closeEasing'       => 'swing',
+                        'closeHtml'         => '<button><i class="icon-off"></i></button>',
+                        'closeMethod'       => 'fadeOut',
+                        'closeOnHover'      => true,
+                        'containerId'       => 'toast-container',
+                        'debug'             => false,
+                        'escapeHtml'        => false,
+                        'extendedTimeOut'   => 10000,
+                        'hideDuration'      => 1000,
+                        'hideEasing'        => 'linear',
+                        'hideMethod'        => 'fadeOut',
+                        'iconClass'         => 'toast-info',
+                        'iconClasses'       => array(
+                            'error'   => 'toast-error',
+                            'info'    => 'toast-info',
+                            'success' => 'toast-success',
+                            'warning' => 'toast-warning',
+                        ),
+                        'messageClass'      => 'toast-message',
+                        'newestOnTop'       => false,
+                        'onHidden'          => null,
+                        'onShown'           => null,
+                        'positionClass'     => 'toast-top-right',
+                        'preventDuplicates' => true,
+                        'progressBar'       => true,
+                        'progressClass'     => 'toast-progress',
+                        'rtl'               => false,
+                        'showDuration'      => 300,
+                        'showEasing'        => 'swing',
+                        'showMethod'        => 'fadeIn',
+                        'tapToDismiss'      => true,
+                        'target'            => 'body',
+                        'timeOut'           => 5000,
+                        'titleClass'        => 'toast-title',
+                        'toastClass'        => 'toast',
+                    ))
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
