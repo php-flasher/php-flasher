8fc15bc52de6c52544f0fe31582529fbdbe2a8ef
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-12-02 00:54:31 +0100

    add php-notify classes and rename namespaces

---
+++ DependencyInjection/NotifySweetAlertExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Flasher\SweetAlert\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class NotifySweetAlertExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        if (!$container->hasExtension('notify')) {
+            throw new \RuntimeException('[Flasher\Symfony\NotifyBundle] is not registered');
+        }
+
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $notifyConfig = $container->getExtensionConfig('notify');
+
+//        dd($notifyConfig);
+
+        $container->prependExtensionConfig('notify', array(
+            'adapters' => array(
+                'sweet_alert' => $config,
+            ),
+        ));
+    }
+}
