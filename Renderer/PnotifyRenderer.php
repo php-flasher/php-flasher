ee936fd8d08a044a211527d0b65a7c1f5ecee2a9
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-12-02 00:54:31 +0100

    add php-notify classes and rename namespaces

---
+++ Renderer/PnotifyRenderer.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Flasher\PFlasher\Prime\TestsRenderer;
+
+use Flasher\Prime\Config\ConfigInterface;
+use Flasher\Prime\Envelope;
+use Flasher\Prime\Renderer\HasGlobalOptionsInterface;
+use Flasher\Prime\Renderer\HasScriptsInterface;
+use Flasher\Prime\Renderer\HasStylesInterface;
+use Flasher\Prime\Renderer\RendererInterface;
+
+class PnotifyRenderer implements RendererInterface, HasScriptsInterface, HasStylesInterface, HasGlobalOptionsInterface
+{
+    /**
+     * @var ConfigInterface
+     */
+    private $config;
+
+    /**
+     * @var array
+     */
+    private $scripts;
+
+    /**
+     * @var array
+     */
+    private $styles;
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    public function __construct(ConfigInterface $config)
+    {
+        $this->config  = $config;
+        $this->scripts = $config->get('adapters.pnotify.scripts', array());
+        $this->styles  = $config->get('adapters.pnotify.styles', array());
+        $this->options = $config->get('adapters.pnotify.options', array());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function render(Envelope $envelope)
+    {
+        $context = $envelope->getContext();
+        $options = isset($context['options']) ? $context['options'] : array();
+
+        $options['title'] = " " . $envelope->getTitle();
+        $options['text'] = $envelope->getMessage();
+        $options['type'] = $envelope->getType();
+
+        return sprintf("new PNotify(%s);", json_encode($options));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getScripts()
+    {
+        return $this->scripts;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getStyles()
+    {
+        return $this->styles;
+    }
+
+    public function renderOptions()
+    {
+        return sprintf('PNotify.defaults = %s;', json_encode($this->options));
+    }
+}
