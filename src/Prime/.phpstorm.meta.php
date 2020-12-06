3a9c6dcf4f2651f8107712bff33b2cd234e5d884
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-06 06:23:34 +0100

    add phpdoc and phpstorm autocomplete

--- src/Prime/.phpstorm.meta.php
+++ src/Prime/.phpstorm.meta.php
@@ -4,11 +4,20 @@ namespace PHPSTORM_META;
 
 use Flasher\Prime\Envelope;
 use Flasher\Prime\Flasher;
+use Flasher\Prime\Notification\NotificationInterface;
 use Flasher\Prime\Presenter\PresenterManager;
 use Flasher\Prime\Renderer\RendererManager;
 
 override(Envelope::get(), type(0));
+override(ManagerInterface::make(0), map(['' => '@']));
 
-override(Flasher::make(''), map(['' => '@']));
-override(RendererManager::make(''), map(['' => '@']));
-override(PresenterManager::make(''), map(['' => '@']));
+registerArgumentsSet('notificationTypes',
+    NotificationInterface::TYPE_SUCCESS,
+    NotificationInterface::TYPE_ERROR,
+    NotificationInterface::TYPE_INFO,
+    NotificationInterface::TYPE_INFO,
+);
+
+expectedArguments(NotificationBuilder::type(), 1, argumentsSet('notificationTypes'));
+expectedReturnValues(NotificationInterface::setType(), 1, argumentsSet('notificationTypes'));
+expectedReturnValues(NotificationInterface::getType(), argumentsSet('notificationTypes'));
--- src/Toastr/Prime/.phpstorm.meta.php
+++ src/Toastr/Prime/.phpstorm.meta.php
@@ -1,3 +1,9 @@
 <?php
 
 namespace PHPSTORM_META;
+
+use Flasher\Toastr\Prime\ToastrBuilder;
+
+expectedArguments(ToastrBuilder::showMethod(), 1, ['fadeIn', 'slideDown', 'show']);
+expectedArguments(ToastrBuilder::showEasing(), 1, ['swing', 'linear']);
+expectedArguments(ToastrBuilder::positionClass(), 1, ['toast-top-right', 'toast-top-center', 'toast-bottom-center', 'toast-top-full-width', 'toast-bottom-full-width', 'toast-top-left', 'toast-bottom-right', 'toast-bottom-left']);
