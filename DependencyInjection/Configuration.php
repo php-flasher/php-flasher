ba8e1bb44c275f064f99c9e197e7f325c1bc1edf
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-06 17:29:32 +0100

    update namespaces

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,13 +12,13 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('notify_sweet-alert');
+        $treeBuilder = new TreeBuilder('flasher_sweet-alert');
 
         if (\method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->getRootNode();
         } else {
             // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('notify_sweet_alert');
+            $rootNode = $treeBuilder->root('flasher_sweet_alert');
         }
 
         $rootNode
--- DependencyInjection/FlasherSweetAlertExtension.php
+++ DependencyInjection/FlasherSweetAlertExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class NotifySweetAlertExtension extends Extension implements PrependExtensionInterface
+final class FlasherSweetAlertExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -18,7 +18,7 @@ final class NotifySweetAlertExtension extends Extension implements PrependExtens
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader->load('config.yaml');
 
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
@@ -29,18 +29,14 @@ final class NotifySweetAlertExtension extends Extension implements PrependExtens
      */
     public function prepend(ContainerBuilder $container)
     {
-        if (!$container->hasExtension('notify')) {
-            throw new \RuntimeException('[Flasher\SymfonyFlasher\PrimeBundle] is not registered');
+        if (!$container->hasExtension('flasher')) {
+            throw new \RuntimeException('[Flasher\Symfony\FlasherBundle] is not registered');
         }
 
         $configs = $container->getExtensionConfig($this->getAlias());
         $config = $this->processConfiguration(new Configuration(), $configs);
 
-        $notifyConfig = $container->getExtensionConfig('notify');
-
-//        dd($notifyConfig);
-
-        $container->prependExtensionConfig('notify', array(
+        $container->prependExtensionConfig('flasher', array(
             'adapters' => array(
                 'sweet_alert' => $config,
             ),
