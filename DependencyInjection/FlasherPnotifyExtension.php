310ab78a7ea56280a62157720e47a1863cf00cdd
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-06 17:29:32 +0100

    update namespaces

--- DependencyInjection/FlasherPnotifyExtension.php
+++ DependencyInjection/FlasherPnotifyExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class NotifyPnotifyExtension extends Extension implements PrependExtensionInterface
+final class FlasherPnotifyExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -18,7 +18,7 @@ final class NotifyPnotifyExtension extends Extension implements PrependExtension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader->load('config.yaml');
 
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
@@ -29,14 +29,14 @@ final class NotifyPnotifyExtension extends Extension implements PrependExtension
      */
     public function prepend(ContainerBuilder $container)
     {
-        if (!$container->hasExtension('notify')) {
-            throw new \RuntimeException('[Flasher\SymfonyFlasher\PrimeBundle] is not registered');
+        if (!$container->hasExtension('flasher')) {
+            throw new \RuntimeException('[Flasher\Symfony\FlasherBundle] is not registered');
         }
 
         $configs = $container->getExtensionConfig($this->getAlias());
         $config = $this->processConfiguration(new Configuration(), $configs);
 
-        $container->prependExtensionConfig('notify', array(
+        $container->prependExtensionConfig('flasher', array(
             'adapters' => array(
                 'pnotify' => $config,
             ),
