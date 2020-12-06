a9c426f5ce1e5d7ae68587e01a8fffbf351e6183
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-06 17:29:32 +0100

    update namespaces

---
+++ NotyfBuilder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Flasher\Notyf\Prime;
+
+use Flasher\Prime\Notification\NotificationBuilder;
+
+/**
+ * @method Pnotify getNotification()
+ */
+final class NotyfBuilder extends NotificationBuilder
+{
+
+}
---
+++ NotyfFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Flasher\Notyf\Prime;
+
+use Flasher\Prime\Factory\AbstractFactory;
+
+final class NotyfFactory extends AbstractFactory
+{
+    /**
+     * @inheritDoc
+     */
+    public function createNotification()
+    {
+        return new Pnotify();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createNotificationBuilder()
+    {
+        return new PnotifyBuilder($this->getEventDispatcher(), $this->createNotification(), 'notyf');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports($name = null, array $context = array())
+    {
+        return in_array($name, array(__CLASS__, 'notyf'));
+    }
+}
