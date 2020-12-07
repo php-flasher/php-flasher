e00cbb77656fa8b2c036551a2a968340b0b32211
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-07 04:13:58 +0100

    convert session flash messages into flasher messages

--- Resources/config/config.php
+++ Resources/config/config.php
@@ -6,4 +6,13 @@ return array(
     'scripts' => array(
         '/vendor/php-flasher/flasher/assets/js/flasher.js'
     ),
+
+    'auto_create_from_session' => true,
+
+    'types_mapping' => array(
+        'success' => array('success'),
+        'error'   => array('error', 'danger'),
+        'warning' => array('warning', 'alarm'),
+        'info'    => array('info', 'notice', 'alert'),
+    ),
 );
