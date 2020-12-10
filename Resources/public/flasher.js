0872391e30225407f6fd4468294267969bab25ec
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-10 04:55:20 +0100

    update flasherjs and flasher symfony bundle

--- Resources/public/flasher.js
+++ Resources/public/flasher.js
@@ -1,49 +1,20 @@
-/*!
- * PHPFlasher js 0.1.0
- * https://github.com/php-flasher/flasher
- * @license MIT licensed
- *
- * Copyright (C) 2020 Younes KHOUBZA
- */
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory(root));
-    } else if (typeof exports === 'object') {
-        module.exports = factory(root);
-    } else {
-        root.PHPFlasher = factory(root);
-    }
-})(typeof global !== 'undefined' ? global : typeof window !== 'undefined' ? window : this, function (window) {
-
+var PHPFlasher = (function () {
     'use strict';
 
     var exports = {};
+    var factories = {};
 
-    exports.render = function (_settings) {
-        var settings = Object.assign({}, {
-            scripts: [],
-            styles: [],
-            options: [],
-            notifications: [],
-        }, _settings);
-
-        exports.addStyles(settings.styles, function () {
-            exports.addScripts(settings.scripts, function () {
-                var script = '';
-
-                settings.options.forEach(function (option) {
-                    script += option + "\n";
-                });
-
-                script += "\n\n";
-
-                settings.notifications.forEach(function (notification) {
-                    script += notification.code + "\n";
-                });
-
-                exports.parseScript(script);
+    exports.render = function (_options) {
+        exports.addStyles(_options.styles, function () {
+            exports.addScripts(_options.scripts, function () {
+                exports.renderOptions(_options.options);
+                exports.renderNotifications(_options.notifications);
             });
         });
+    };
+
+    exports.addFactory = function (alias, factory) {
+        factories[alias] = factory;
     };
 
     exports.addStyles = function (urls, callback) {
@@ -55,7 +26,7 @@
             return this;
         }
 
-        if (null !== document.querySelector(`link[href='${urls[0]}']`)) {
+        if (null !== document.querySelector('link[href="' + urls[0] + '"]')) {
             return exports.addStyles(urls.slice(1), callback);
         }
 
@@ -82,7 +53,7 @@
             return this;
         }
 
-        if (null !== document.querySelector(`script[src='${urls[0]}']`)) {
+        if (null !== document.querySelector('script[src="' + urls[0] + '"]')) {
             return exports.addScripts(urls.slice(1), callback);
         }
 
@@ -99,16 +70,17 @@
         return this;
     };
 
-    exports.parseScript = function (script) {
-        var tag = document.createElement('script');
+    exports.renderOptions = function (options) {
+        Object.keys(options).forEach(function (library) {
+            factories[library].renderOptions(options[library]);
+        });
+    };
 
-        tag.type = "text/javascript";
-        tag.text = script;
-
-        document.body.appendChild(tag);
-
-        return this;
+    exports.renderNotifications = function (notifications) {
+        notifications.forEach(function (flash) {
+            factories[flash.handler].render(flash.notification);
+        })
     };
 
     return exports;
-});
+})();
