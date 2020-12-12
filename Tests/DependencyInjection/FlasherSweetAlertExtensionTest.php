66e3ff54598ba34984158d4464c85386fdebbcfb
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-12 09:23:06 +0100

    add sweet alert symfony tests

---
+++ Tests/DependencyInjection/FlasherSweetAlertExtensionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Flasher\SweetAlert\Symfony\Tests\DependencyInjection;
+
+use Flasher\Prime\Tests\TestCase;
+use Flasher\Symfony\DependencyInjection\FlasherExtension;
+use Flasher\Symfony\FlasherBundle;
+use Flasher\SweetAlert\Symfony\DependencyInjection\FlasherSweetAlertExtension;
+use Flasher\SweetAlert\Symfony\FlasherSweetAlertBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class FlasherSweetAlertExtensionTest extends TestCase
+{
+    public function testContainerContainFlasherServices()
+    {
+        $container = $this->getRawContainer();
+        $container->loadFromExtension('flasher', array());
+        $container->loadFromExtension('flasher_sweet_alert', array());
+        $container->compile();
+
+        $this->assertTrue($container->has('flasher.sweet_alert'));
+    }
+
+    public function testCreateInstanceOfSweetAlertAdapter()
+    {
+        $container = $this->getRawContainer();
+        $container->loadFromExtension('flasher');
+        $container->loadFromExtension('flasher_sweet_alert');
+        $container->compile();
+
+        $flasher = $container->getDefinition('flasher');
+        $calls = $flasher->getMethodCalls();
+
+        $this->assertCount(1, $calls);
+        $this->assertSame('addFactory', $calls[0][0]);
+        $this->assertSame('sweet_alert', $calls[0][1][0]);
+        $this->assertSame('flasher.sweet_alert', (string) $calls[0][1][1]);
+    }
+
+    public function testConfigurationInjectedIntoFlasherConfig()
+    {
+        $container = $this->getContainer();
+        $config = $container->get('flasher.config');
+        $this->assertNotEmpty($config->get('adapters.sweet_alert'));
+    }
+
+    private function getRawContainer()
+    {
+        $container = new ContainerBuilder();
+
+        $container->registerExtension(new FlasherExtension());
+        $flasherBundle = new FlasherBundle();
+        $flasherBundle->build($container);
+
+        $container->registerExtension(new FlasherSweetAlertExtension());
+        $adapterBundle = new FlasherSweetAlertBundle();
+        $adapterBundle->build($container);
+
+        $container->getCompilerPassConfig()->setOptimizationPasses(array());
+        $container->getCompilerPassConfig()->setRemovingPasses(array());
+        $container->getCompilerPassConfig()->setAfterRemovingPasses(array());
+
+        return $container;
+    }
+
+    private function getContainer()
+    {
+        $container = $this->getRawContainer();
+        $container->compile();
+
+        return $container;
+    }
+}
