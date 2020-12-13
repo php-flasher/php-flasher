88bedffdca9a0d13f01e4ff31b789abe3fe74d9d
Khoubza Younes <younes.khoubza@smile-maroc.com>
2020-12-13 07:33:03 +0100

    resolve circular dependecy between event_dispatcher and storage_manager

--- Tests/FlasherNotyServiceProviderTest.php
+++ Tests/FlasherNotyServiceProviderTest.php
@@ -6,38 +6,54 @@ class FlasherNotyServiceProviderTest extends TestCase
 {
     public function testContainerContainNotifyServices()
     {
-        $this->assertTrue($this->app->bound('flasher'));
-        $this->assertTrue($this->app->bound('flasher.factory.noty'));
+        $this->assertTrue($this->app->bound('flasher.noty'));
+        $this->assertInstanceOf('Flasher\Noty\Prime\NotyFactory', $this->app->make('flasher.noty'));
     }
 
     public function testNotifyFactoryIsAddedToExtensionsArray()
     {
         $flasher = $this->app->make('flasher');
+        $adapter = $flasher->create('noty');
 
-        $reflection = new \ReflectionClass($flasher);
-        $property = $reflection->getProperty('drivers');
-        $property->setAccessible(true);
-
-        $extensions = $property->getValue($flasher);
-
-        $this->assertCount(1, $extensions);
-        $this->assertInstanceOf('Flasher\Prime\Factory\FactoryInterface', $extensions[0]);
+        $this->assertInstanceOf('Flasher\Noty\Prime\NotyFactory', $adapter);
     }
 
     public function testConfigNotyInjectedInGlobalNotifyConfig()
     {
-        $flasher = $this->app->make('flasher');
+        $config = $this->app->make('flasher.config');
 
-        $reflection = new \ReflectionClass($flasher);
-        $property = $reflection->getProperty('config');
-        $property->setAccessible(true);
+        $expected = array(
+            'noty' => array(
+                'scripts' => array(
+                    'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.js',
+                ),
+                'styles'  => array(
+                    'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
+                ),
+                'options' => array(
+                    'layout'              => 'topRight',
+                    'theme'               => 'mint',
+                    'timeout'             => 5000,
+                    'progressBar'         => true,
+                    'animation.open'      => 'noty_effects_open',
+                    'animation.close'     => 'noty_effects_close',
+                    'sounds.sources'      => array(),
+                    'closeWith'           => array('click'),
+                    'sounds.volume'       => 1,
+                    'sounds.conditions'   => array(),
+                    'docTitle.conditions' => array(),
+                    'modal'               => false,
+                    'id'                  => false,
+                    'force'               => false,
+                    'queue'               => 'global',
+                    'killer'              => false,
+                    'container'           => false,
+                    'buttons'             => array(),
+                    'visibilityControl'   => false,
+                ),
+            ),
+        );
 
-        $config = $property->getValue($flasher);
-
-        $this->assertArrayHasKey('noty', $config->get('adapters'));
-
-        $this->assertEquals(array(
-            'noty' => array('scripts' => array('jquery.js'), 'styles' => array('styles.css'), 'options' => array()),
-        ), $config->get('adapters'));
+        $this->assertEquals($expected, $config->get('adapters'));
     }
 }
