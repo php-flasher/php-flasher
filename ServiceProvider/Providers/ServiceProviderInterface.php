922460389577419b5e42d9f2e3716e73077b5d8b
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-12-17 00:27:12 +0100

    remove lumen support and clean up service provider

--- ServiceProvider/Providers/ServiceProviderInterface.php
+++ ServiceProvider/Providers/ServiceProviderInterface.php
@@ -6,11 +6,18 @@ use Flasher\Notyf\Laravel\FlasherNotyfServiceProvider;
 
 interface ServiceProviderInterface
 {
+    /**
+     * @return bool
+     */
     public function shouldBeUsed();
 
-    public function publishConfig(FlasherNotyfServiceProvider $provider);
+    /**
+     * @param FlasherNotyfServiceProvider $provider
+     */
+    public function boot(FlasherNotyfServiceProvider $provider);
 
-    public function registerServices();
-
-    public function mergeConfigFromNotyf();
+    /**
+     * @param FlasherNotyfServiceProvider $provider
+     */
+    public function register(FlasherNotyfServiceProvider $provider);
 }
