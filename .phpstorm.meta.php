cf77cfd0f3bdeb2932dcf606d198e59e17198ecc
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-01-09 08:17:47 +0100

    addoptions to flasher libraries builders

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,15 +2,12 @@
 
 namespace PHPSTORM_META;
 
-use Flasher\Prime\Notification\NotificationBuilderInterface;
-use Flasher\Prime\Notification\NotificationInterface;
-
 registerArgumentsSet('notificationTypes', NotificationInterface::TYPE_SUCCESS, NotificationInterface::TYPE_ERROR, NotificationInterface::TYPE_WARNING, NotificationInterface::TYPE_INFO);
 
 override(Envelope::get(0), type(0));
 
-expectedArguments(NotificationBuilderInterface::type(), 1, argumentsSet('notificationTypes'));
-expectedArguments(NotificationBuilderInterface::addFlash(), 1, argumentsSet('notificationTypes'));
-expectedArguments(NotificationInterface::setType(), 1, argumentsSet('notificationTypes'));
+expectedArguments(\Flasher\Prime\Notification\NotificationBuilderInterface::type(), 0, argumentsSet('notificationTypes'));
+expectedArguments(\Flasher\Prime\Notification\NotificationBuilderInterface::addFlash(), 0, argumentsSet('notificationTypes'));
+expectedArguments(\Flasher\Prime\Notification\NotificationInterface::setType(), 0, argumentsSet('notificationTypes'));
 
-expectedReturnValues(NotificationInterface::getType(), argumentsSet('notificationTypes'));
+expectedReturnValues(\Flasher\Prime\Notification\NotificationInterface::getType(), argumentsSet('notificationTypes'));
