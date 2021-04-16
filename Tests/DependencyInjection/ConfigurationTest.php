ac3b15d015104aa1d6cba405bc814881c612b7d1
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-16 20:08:36 +0200

    clean up code

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -14,32 +14,11 @@ class ConfigurationTest extends TestCase
 
         $expected = array(
             'scripts' => array(
-                'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.js',
-                '/bundles/flashernoty/flasher-noty.js',
+                'https://cdn.jsdelivr.net/npm/@flasher/flasher-noty@0.1.3/dist/flasher-noty.min.js',
             ),
-            'styles'  => array(
-                'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css',
-            ),
+            'styles'  => array(),
             'options' => array(
-                'layout'              => 'topRight',
-                'theme'               => 'mint',
-                'timeout'             => false,
-                'progressBar'         => true,
-                'animation.open'      => 'noty_effects_open',
-                'animation.close'     => 'noty_effects_close',
-                'sounds.sources'      => array(),
-                'closeWith'           => array('click'),
-                'sounds.volume'       => 1,
-                'sounds.conditions'   => array(),
-                'docTitle.conditions' => array(),
-                'modal'               => false,
-                'id'                  => false,
-                'force'               => false,
-                'queue'               => 'global',
-                'killer'              => false,
-                'container'           => false,
-                'buttons'             => array(),
-                'visibilityControl'   => false,
+                'timeout' => 5000,
             ),
         );
 
