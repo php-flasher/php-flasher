ad1f718e50e9cbc7259fe33b345f44aa91279179
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-17 21:32:30 +0200

    update laravel tests

--- Tests/FlasherSweetAlertServiceProviderTest.php
+++ Tests/FlasherSweetAlertServiceProviderTest.php
@@ -17,23 +17,4 @@ class FlasherSweetAlertServiceProviderTest extends TestCase
 
         $this->assertInstanceOf('Flasher\SweetAlert\Prime\SweetAlertFactory', $adapter);
     }
-
-    public function testConfigNotyInjectedInGlobalNotifyConfig()
-    {
-        $config = $this->app->make('flasher.config');
-
-        $expected = array(
-            'scripts' => array(
-                'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-                'https://cdn.jsdelivr.net/npm/@flasher/flasher-sweet-alert@0.1.3/dist/flasher-sweet-alert.min.js',
-            ),
-            'styles'  => array(),
-            'options' => array(
-                'timer' => 50000,
-                'timerProgressBar' => true,
-            ),
-        );
-
-        $this->assertEquals($expected, $config->get('adapters.sweet_alert'));
-    }
 }
