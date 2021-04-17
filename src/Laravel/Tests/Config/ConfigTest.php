87e668b51753f43c0001c64ac35b54d1ffe61fee
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-17 21:32:30 +0200

    update laravel tests

--- src/Laravel/Tests/Config/ConfigTest.php
+++ src/Laravel/Tests/Config/ConfigTest.php
@@ -14,39 +14,6 @@ final class ConfigTest extends TestCase
         $config = new Config($this->app->make('config'), $separator);
 
         $this->assertEquals('template', $config->get('default'));
-        $this->assertSame(array(
-            'template' => array(
-                'default'   => 'tailwindcss',
-                'templates' => array(
-                    'tailwindcss'    => array(
-                        'view'   => 'flasher::tailwindcss',
-                        'styles' => array(
-                            'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css',
-                        ),
-                    ),
-                    'tailwindcss_bg' => array(
-                        'view'   => 'flasher::tailwindcss_bg',
-                        'styles' => array(
-                            'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css',
-                        ),
-                    ),
-                    'bootstrap'      => array(
-                        'view'   => 'flasher::bootstrap',
-                        'styles' => array(
-                            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css',
-                        ),
-                    ),
-                ),
-                'scripts'   => array(
-                    '/vendor/flasher/flasher-template.js',
-                ),
-                'styles'    => array(),
-                'options'   => array(
-                    'timeout'  => 5000,
-                    'position' => 'top-right',
-                ),
-            ),
-        ), $config->get('adapters', array()));
     }
 
     private function isLaravel4()
--- src/Laravel/Tests/FlasherServiceProviderTest.php
+++ src/Laravel/Tests/FlasherServiceProviderTest.php
@@ -10,7 +10,8 @@ final class FlasherServiceProviderTest extends TestCase
     {
         $this->assertInstanceOf('Flasher\Laravel\Config\Config', $this->app->make('flasher.config'));
         $this->assertInstanceOf('Flasher\Prime\Flasher', $this->app->make('flasher'));
-        $this->assertInstanceOf('Flasher\Prime\Renderer\Renderer', $this->app->make('flasher.response_manager'));
+        $this->assertInstanceOf('Flasher\Prime\Response\ResponseManager', $this->app->make('flasher.response_manager'));
+        $this->assertInstanceOf('Flasher\Prime\Response\Resource\ResourceManager', $this->app->make('flasher.resource_manager'));
         $this->assertInstanceOf('Flasher\Laravel\Storage\Storage', $this->app->make('flasher.storage'));
         $this->assertInstanceOf('Flasher\Prime\Storage\StorageManager', $this->app->make('flasher.storage_manager'));
         $this->assertInstanceOf('Flasher\Prime\EventDispatcher\EventDispatcher', $this->app->make('flasher.event_dispatcher'));
@@ -22,9 +23,9 @@ final class FlasherServiceProviderTest extends TestCase
         /** @var BladeCompiler $blade */
         $blade = $this->app->make('view')->getEngineResolver()->resolve('blade')->getCompiler();
 
-        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(), array('format' => 'html')); ?>", $blade->compileString('@flasher_render'));
-        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(), array('format' => 'html')); ?>", $blade->compileString('@flasher_render()'));
-        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(), array('format' => 'html')); ?>", $blade->compileString('@flasher_render(array())'));
-        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(\"priority\" => array(\"min\" => 4, \"max\" => 5)), array('format' => 'html')); ?>", $blade->compileString('@flasher_render(array("priority" => array("min" => 4, "max" => 5)))'));
+        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(), 'html'); ?>", $blade->compileString('@flasher_render'));
+        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(), 'html'); ?>", $blade->compileString('@flasher_render()'));
+        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(), 'html'); ?>", $blade->compileString('@flasher_render(array())'));
+        $this->assertEquals("<?php echo app('flasher.response_manager')->render(array(\"priority\" => array(\"min\" => 4, \"max\" => 5)), 'html'); ?>", $blade->compileString('@flasher_render(array("priority" => array("min" => 4, "max" => 5)))'));
     }
 }
