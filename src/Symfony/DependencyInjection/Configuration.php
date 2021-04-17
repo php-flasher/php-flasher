6f056d5b39bb57a7d06c624f85a20a69c623b975
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-17 18:22:16 +0200

    update twig template namespace

--- src/Symfony/DependencyInjection/Configuration.php
+++ src/Symfony/DependencyInjection/Configuration.php
@@ -55,21 +55,21 @@ final class Configuration implements ConfigurationInterface
                             ->end()
                             ->defaultValue(array(
                                 'tailwindcss'    => array(
-                                    'view'   => '@Flasher/tailwindcss.html.twig',
+                                    'view'   => '@FlasherSymfony/tailwindcss.html.twig',
                                     'styles' => array(
                                         'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.1/base.min.css',
                                         'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.1/utilities.css',
                                     ),
                                 ),
                                 'tailwindcss_bg' => array(
-                                    'view'   => '@Flasher/tailwindcss_bg.html.twig',
+                                    'view'   => '@FlasherSymfony/tailwindcss_bg.html.twig',
                                     'styles' => array(
                                         'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.1/base.min.css',
                                         'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.1/utilities.css',
                                     ),
                                 ),
                                 'bootstrap'      => array(
-                                    'view'   => '@Flasher/bootstrap.html.twig',
+                                    'view'   => '@FlasherSymfony/bootstrap.html.twig',
                                     'styles' => array(
                                         'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css',
                                     ),
