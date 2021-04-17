a2c28cf57ec0f529f9b473099b91f2d5bf9d18ee
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-17 21:32:30 +0200

    update laravel tests

--- Tests/FlasherNotyfServiceProviderTest.php
+++ Tests/FlasherNotyfServiceProviderTest.php
@@ -17,35 +17,4 @@ class FlasherNotyfServiceProviderTest extends TestCase
 
         $this->assertInstanceOf('Flasher\Notyf\Prime\NotyfFactory', $adapter);
     }
-
-    public function testConfigNotyInjectedInGlobalNotifyConfig()
-    {
-        $config = $this->app->make('flasher.config');
-
-        $expected = array(
-            'scripts' => array(
-                'https://cdn.jsdelivr.net/npm/@flasher/flasher-notyf@0.1.5/dist/flasher-notyf.min.js',
-            ),
-            'styles' => array(),
-            'options' => array(
-                'duration' => 5000,
-                'types' => array(
-                    array(
-                        'type'            => 'info',
-                        'className'       => 'notyf__toast--info',
-                        'backgroundColor' => '#5784E5',
-                        'icon'            => false,
-                    ),
-                    array(
-                        'type'            => 'warning',
-                        'className'       => 'notyf__toast--warning',
-                        'backgroundColor' => '#E3A008',
-                        'icon'            => false,
-                    )
-                ),
-            ),
-        );
-
-        $this->assertEquals($expected, $config->get('adapters.notyf'));
-    }
 }
