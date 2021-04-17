76131020393a99c02feab214fb56a5efc4cde2a6
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-17 21:32:30 +0200

    update laravel tests

--- Tests/FlasherToastrServiceProviderTest.php
+++ Tests/FlasherToastrServiceProviderTest.php
@@ -17,23 +17,4 @@ class FlasherToastrServiceProviderTest extends TestCase
 
         $this->assertInstanceOf('Flasher\Toastr\Prime\ToastrFactory', $adapter);
     }
-
-    public function testConfigNotyInjectedInGlobalNotifyConfig()
-    {
-        $config = $this->app->make('flasher.config');
-
-        $expected = array(
-            'scripts' => array(
-                'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-                'https://cdn.jsdelivr.net/npm/@flasher/flasher-toastr@0.1.3/dist/flasher-toastr.min.js',
-            ),
-            'styles' => array(),
-            'options' => array(
-                'progressBar' => true,
-                'timeOut' => 5000,
-            ),
-        );
-
-        $this->assertEquals($expected, $config->get('adapters.toastr'));
-    }
 }
