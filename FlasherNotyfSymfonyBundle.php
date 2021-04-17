926cb9fe704c042bb6c5396efc5e3955d2dd7bdf
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-17 18:40:02 +0200

    rename extensions

--- FlasherNotyfSymfonyBundle.php
+++ FlasherNotyfSymfonyBundle.php
@@ -2,8 +2,13 @@
 
 namespace Flasher\Notyf\Symfony;
 
+use Flasher\Notyf\Symfony\DependencyInjection\FlasherNotyfExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FlasherNotyfSymfonyBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new FlasherNotyfExtension();
+    }
 }
