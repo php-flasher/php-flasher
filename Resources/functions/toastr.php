0ccfc98476774bbf184ff1e60e59217ff6b8b340
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-04-25 10:09:31 +0000

    add laravel helpers and fix blade compiler for laravel 4

---
+++ Resources/functions/toastr.php
@@ -0,0 +1,25 @@
+<?php
+
+use Flasher\Prime\Notification\NotificationInterface;
+use Illuminate\Support\Facades\App;
+
+if (! function_exists('toastr'))  {
+    /**
+     * @param string $message
+     * @param string $type
+     * @param string $title
+     * @param array  $options
+     *
+     * @return \Flasher\Toastr\Prime\ToastrFactory
+     */
+    function toastr($message = null, $type = NotificationInterface::TYPE_SUCCESS, $title = '', array $options = array())
+    {
+        if (null === $message) {
+            return App::make('flasher.toastr');
+        }
+
+        return App::make('flasher.toastr')
+            ->title($title)
+            ->addFlash($type, $message, $options);
+    }
+}
