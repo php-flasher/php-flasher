f72bcb2b107da3f437b14c1d552f5546d273e115
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-05-06 11:51:32 +0000

    update laravel configuration

--- src/Notyf/Symfony/Tests/DependencyInjection/ConfigurationTest.php
+++ src/Notyf/Symfony/Tests/DependencyInjection/ConfigurationTest.php
@@ -19,20 +19,6 @@ class ConfigurationTest extends TestCase
             'styles' => array(),
             'options' => array(
                 'duration' => 5000,
-                'types' => array(
-                    array(
-                        'type'            => 'info',
-                        'className'       => 'notyf__toast--info',
-                        'backgroundColor' => '#5784E5',
-                        'icon'            => false,
-                    ),
-                    array(
-                        'type'            => 'warning',
-                        'className'       => 'notyf__toast--warning',
-                        'backgroundColor' => '#E3A008',
-                        'icon'            => false,
-                    )
-                ),
             ),
         );
 
