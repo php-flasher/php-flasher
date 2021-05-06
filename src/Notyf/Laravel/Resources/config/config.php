dcaec65036c54a987ded31aadb53ff315be781d8
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-05-06 11:51:32 +0000

    update laravel configuration

--- src/Notyf/Laravel/Resources/config/config.php
+++ src/Notyf/Laravel/Resources/config/config.php
@@ -2,24 +2,10 @@
 
 return array(
     'scripts' => array(
-        'https://cdn.jsdelivr.net/npm/@flasher/flasher-notyf@0.1.5/dist/flasher-notyf.min.js',
+        'https://cdn.jsdelivr.net/npm/@flasher/flasher-notyf@0.1.6/dist/flasher-notyf.min.js',
     ),
     'styles' => array(),
     'options' => array(
         'duration' => 5000,
-        'types' => array(
-            array(
-                'type'            => 'info',
-                'className'       => 'notyf__toast--info',
-                'backgroundColor' => '#5784E5',
-                'icon'            => false,
-            ),
-            array(
-                'type'            => 'warning',
-                'className'       => 'notyf__toast--warning',
-                'backgroundColor' => '#E3A008',
-                'icon'            => false,
-            )
-        ),
     ),
 );
