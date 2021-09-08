bafe70abab715b97b52c8d17a4c0f86a80d2e5d7
Khoubza Younes <younes.khoubza@smile-maroc.com>
2021-09-08 08:23:43 +0100

    emit livewire events on sweet alert promise

--- src/Livewire/LivewireResponseManager.php
+++ src/Livewire/LivewireResponseManager.php
@@ -39,6 +39,37 @@ JAVASCRIPT;
     window.addEventListener('flasher:render', function (event) {
         Flasher.getInstance().render(event.detail);
     });
+
+    window.addEventListener('flasher:sweet_alert:promise', function (event) {
+        var envelope = event.detail.envelope;
+        var context = envelope.context;
+
+        if (!context.livewire || !context.livewire.id) {
+            return;
+        }
+
+        var detail = event.detail;
+        if (envelope.livewire_context) {
+            detail.context = envelope.livewire_context;
+        }
+
+        var component = context.livewire.id;
+
+        Livewire.components.emitSelf(component, 'sweetAlertEvent', detail);
+
+        var promise = event.detail.promise;
+        if (promise.isConfirmed) {
+            Livewire.components.emitSelf(component, 'sweetAlertConfirmed', detail);
+        }
+
+        if (promise.isDenied) {
+            Livewire.components.emitSelf(component, 'sweetAlertDenied', detail);
+        }
+
+        if (promise.isDismissed) {
+            Livewire.components.emitSelf(component, 'sweetAlertDismissed', detail);
+        }
+    }, false);
 </script>
 JAVASCRIPT;
     }
