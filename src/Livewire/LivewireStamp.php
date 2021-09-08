28bb4e22b82e2e8e2b4c1cb3c224c5104c3b397c
Khoubza Younes <younes.khoubza@smile-maroc.com>
2021-09-08 08:28:01 +0100

    add context to livewire Stamp

--- src/Livewire/LivewireStamp.php
+++ src/Livewire/LivewireStamp.php
@@ -4,8 +4,27 @@ declare(strict_types=1);
 
 namespace Flasher\Livewire;
 
+use Flasher\Prime\Stamp\PresentableStampInterface;
 use Flasher\Prime\Stamp\StampInterface;
 
-class LivewireStamp implements StampInterface
+class LivewireStamp implements StampInterface, PresentableStampInterface
 {
+    private $context;
+
+    public function __construct(array $context = [])
+    {
+        $this->context = $context;
+    }
+
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    public function toArray()
+    {
+        return [
+            'livewire_context' => $this->getContext(),
+        ];
+    }
 }
