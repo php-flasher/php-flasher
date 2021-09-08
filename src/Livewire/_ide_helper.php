0384f9ff35a06ab533f735886f6b491213e9259a
Khoubza Younes <younes.khoubza@smile-maroc.com>
2021-09-08 08:24:29 +0100

    add phpstorm autocomplete for livewire macro

--- src/Livewire/_ide_helper.php
+++ src/Livewire/_ide_helper.php
@@ -5,8 +5,37 @@ namespace  {
 }
 
 namespace Flasher\Prime\Notification {
-    interface NotificationBuilderInterface
-    {
-        public function livewire(): self;
+    interface NotificationBuilderInterface {
+        public function livewire(array $context = []): self;
+    }
+}
+
+namespace Flasher\SweetAlert\Prime {
+    class SweetAlertFactory {
+        public function livewire(array $context = []): self { }
+    }
+}
+
+namespace Flasher\Toastr\Prime {
+    class ToastrFactory {
+        public function livewire(array $context = []): self { }
+    }
+}
+
+namespace Flasher\Noty\Prime {
+    class NotyFactory {
+        public function livewire(array $context = []): self { }
+    }
+}
+
+namespace Flasher\Notyf\Prime {
+    class NotyfFactory {
+        public function livewire(array $context = []): self { }
+    }
+}
+
+namespace Flasher\Pnotify\Prime {
+    class PnotifyFactory {
+        public function livewire(array $context = []): self { }
     }
 }
