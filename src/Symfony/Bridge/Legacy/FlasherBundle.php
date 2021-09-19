402953f2ff478717bd623bcd0be0e9a40c9300b4
Khoubza Younes <younes.khoubza@smile-maroc.com>
2021-09-19 16:24:19 +0100

    fix legacy symfony supports

--- src/Symfony/Bridge/Legacy/FlasherBundle.php
+++ src/Symfony/Bridge/Legacy/FlasherBundle.php
@@ -3,12 +3,21 @@
 namespace Flasher\Symfony\Bridge\Legacy;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 abstract class FlasherBundle extends Bundle
 {
-    abstract protected function flasherBuild(ContainerBuilder $container);
+    /**
+     * @return void
+     */
+    protected function flasherBuild(ContainerBuilder $container)
+    {
+    }
 
+    /**
+     * @return ?ExtensionInterface
+     */
     abstract protected function getFlasherContainerExtension();
 
     public function build(ContainerBuilder $container)
