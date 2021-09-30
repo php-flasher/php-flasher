7fb7056ff821e53fce7420f334044271ff2b995f
Khoubza Younes <younes.khoubza@smile-maroc.com>
2021-09-30 12:14:18 +0100

    update laravel config

--- src/Cli/Laravel/Resources/config/config.php
+++ src/Cli/Laravel/Resources/config/config.php
@@ -1,18 +1,90 @@
 <?php
 
 return array(
+    'render_all' => true,
+
+    'render_immediately' => true,
+
     'title' => 'PHP Flasher',
 
     'mute' => false,
 
     'filter_criteria' => array(),
 
-    'notify_send' => array(
-        'binary' => 'notify-send',
-        'expire_time' => 0,
-        'enabled' => true,
-        'icons' => array(),
-    ),
-
     'icons' => array(),
+
+    'sounds' => array(),
+
+    'notifiers' => array(
+        'growl_notify' => array(
+            'enabled' => true,
+            'priority' => 0,
+            'binary' => 'growlnotify',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+        'kdialog_notifier' => array(
+            'enabled' => true,
+            'priority' => 0,
+            'binary' => 'kdialog',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+        'notifu_notifier' => array(
+            'enabled' => true,
+            'priority' => 0,
+            'binary' => 'notifu',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+        'notify_send' => array(
+            'enabled' => true,
+            'priority' => 1,
+            'binary' => 'notify-send',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+        'snore_toast_notifier' => array(
+            'enabled' => true,
+            'priority' => 0,
+            'binary' => 'snoretoast',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+        'terminal_notifier_notifier' => array(
+            'enabled' => true,
+            'priority' => 0,
+            'binary' => 'terminal-notifier',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+        'toaster' => array(
+            'enabled' => true,
+            'priority' => 0,
+            'binary' => 'toast',
+            'binary_paths' => array(),
+            'title' => null,
+            'mute' => true,
+            'icons' => array(),
+            'sounds' => array(),
+        ),
+    ),
 );
--- src/Cli/Laravel/ServiceProvider/Providers/Laravel.php
+++ src/Cli/Laravel/ServiceProvider/Providers/Laravel.php
@@ -2,11 +2,20 @@
 
 namespace Flasher\Cli\Laravel\ServiceProvider\Providers;
 
-use Flasher\Cli\Prime\EventListener\RenderListener;
-use Flasher\Cli\Prime\FlasherCli;
-use Flasher\Cli\Prime\Notifier\NotifySendNotifier;
 use Flasher\Cli\Laravel\FlasherCliServiceProvider;
+use Flasher\Cli\Prime\CliNotificationFactory;
+use Flasher\Cli\Prime\EventListener\RenderListener;
+use Flasher\Cli\Prime\EventListener\StampsListener;
+use Flasher\Cli\Prime\Notifier\GrowlNotifyNotifier;
+use Flasher\Cli\Prime\Notifier\KDialogNotifier;
+use Flasher\Cli\Prime\Notifier\NotifuNotifier;
+use Flasher\Cli\Prime\Notifier\NotifySendNotifier;
+use Flasher\Cli\Prime\Notifier\SnoreToastNotifier;
+use Flasher\Cli\Prime\Notifier\TerminalNotifierNotifier;
+use Flasher\Cli\Prime\Notifier\ToasterNotifier;
+use Flasher\Cli\Prime\Presenter\CliPresenter;
 use Flasher\Prime\EventDispatcher\EventDispatcher;
+use Flasher\Prime\Response\ResponseManager;
 use Illuminate\Container\Container;
 use Illuminate\Foundation\Application;
 
@@ -30,45 +39,90 @@ class Laravel implements ServiceProviderInterface
     public function boot(FlasherCliServiceProvider $provider)
     {
         $provider->publishes(array(
-            flasher_path(__DIR__ . '/../../Resources/config/config.php') => config_path('flasher_cli.php'),
+            flasher_path(__DIR__.'/../../Resources/config/config.php') => config_path('flasher_cli.php'),
         ), 'flasher-config');
+
+        $this->app['flasher.response_manager']->addPresenter('cli', $this->app['flasher.presenter.cli']);
+        $this->app['flasher.event_dispatcher']->addSubscriber(new RenderListener($this->app['flasher.cli']));
+        $this->app['flasher.event_dispatcher']->addSubscriber(new StampsListener(
+            $this->app['config']->get('flasher_cli.render_all', false),
+            $this->app['config']->get('flasher_cli.render_immediately', true)
+        ));
+
     }
 
     public function register(FlasherCliServiceProvider $provider)
     {
-        $provider->mergeConfigFrom(flasher_path(__DIR__ . '/../../Resources/config/config.php'), 'flasher_cli');
+        $provider->mergeConfigFrom(flasher_path(__DIR__.'/../../Resources/config/config.php'), 'flasher_cli');
 
         $this->registerServices();
     }
 
     public function registerServices()
     {
-        $this->app->singleton('flasher.cli.notify_send', function (Container $app) {
-            $options = $app['config']->get('flasher_cli.notify_send', array());
-
-            $options['icons'] = array_replace_recursive($app['config']->get('flasher_cli.icons', array()), $options['icons']);
-            $options['title'] = $app['config']->get('flasher_cli.title', null);
-            $options['mute'] = $app['config']->get('flasher_cli.mute', true);
-
-            return new NotifySendNotifier($options);
-        });
-
-
-
         $this->app->singleton('flasher.cli', function (Container $app) {
-            $cli = new FlasherCli();
-
-            $cli->addNotifier($app['flasher.cli.notify_send']);
-
-            return $cli;
+            return new CliNotificationFactory(
+                $app['flasher.storage_manager'],
+                $app['flasher.response_manager'],
+                $app['config']->get('flasher_cli.filter_criteria', array())
+            );
         });
 
-        $this->app->extend('flasher.event_dispatcher', function (EventDispatcher $eventDispatcher, Container $app) {
-            $eventDispatcher->addSubscriber(new RenderListener($app['flasher.cli']));
+        $this->app->singleton('flasher.cli.growl_notify', function (Container $app) {
+            return new GrowlNotifyNotifier(Laravel::createConfigFor($app, 'growl_notify'));
+        });
 
-            return $eventDispatcher;
+        $this->app->singleton('flasher.cli.kdialog_notifier', function (Container $app) {
+            return new KDialogNotifier(Laravel::createConfigFor($app, 'kdialog_notifier'));
+        });
+
+        $this->app->singleton('flasher.cli.notifu_notifier', function (Container $app) {
+            return new NotifuNotifier(Laravel::createConfigFor($app, 'notifu_notifier'));
+        });
+
+        $this->app->singleton('flasher.cli.notify_send', function (Container $app) {
+            return new NotifySendNotifier(Laravel::createConfigFor($app, 'notify_send'));
+        });
+
+        $this->app->singleton('flasher.cli.snore_toast_notifier', function (Container $app) {
+            return new SnoreToastNotifier(Laravel::createConfigFor($app, 'snore_toast_notifier'));
+        });
+
+        $this->app->singleton('flasher.cli.terminal_notifier_notifier', function (Container $app) {
+            return new TerminalNotifierNotifier(Laravel::createConfigFor($app, 'terminal_notifier_notifier'));
+        });
+
+        $this->app->singleton('flasher.cli.toaster', function (Container $app) {
+            return new ToasterNotifier(Laravel::createConfigFor($app, 'toaster'));
+        });
+
+        $this->app->singleton('flasher.presenter.cli', function (Container $app) {
+            $presenter = new CliPresenter();
+
+            $presenter->addNotifier($app['flasher.cli.growl_notify']);
+            $presenter->addNotifier($app['flasher.cli.kdialog_notifier']);
+            $presenter->addNotifier($app['flasher.cli.notifu_notifier']);
+            $presenter->addNotifier($app['flasher.cli.notify_send']);
+            $presenter->addNotifier($app['flasher.cli.snore_toast_notifier']);
+            $presenter->addNotifier($app['flasher.cli.terminal_notifier_notifier']);
+            $presenter->addNotifier($app['flasher.cli.toaster']);
+
+            return $presenter;
         });
 
         $this->app->alias('flasher.cli', 'Flasher\Cli\Prime\FlasherCli');
+        $this->app->alias('Flasher\Cli\Prime\FlasherCli', 'Flasher\Cli\Prime\CliFlasherInterface');
+    }
+
+    private static function createConfigFor(Container $app, $notifier)
+    {
+        $options = $app['config']->get('flasher_cli.notifiers.'.$notifier);
+
+        $options['title'] = $app['config']->get('flasher_cli.title');
+        $options['mute'] = $app['config']->get('flasher_cli.mute');
+        $options['icons'] = array_replace_recursive($app['config']->get('flasher_cli.icons'), $options['icons']);
+        $options['sounds'] = array_replace_recursive($app['config']->get('flasher_cli.sounds'), $options['sounds']);
+
+        return $options;
     }
 }
