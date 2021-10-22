f9330057a375e096fe59a83e30afb9c343db82bd
Khoubza Younes <younes.khoubza@smile-maroc.com>
2021-10-22 23:19:33 +0100

    add flash helper method for template factory

--- src/Laravel/helpers.php
+++ src/Laravel/helpers.php
@@ -19,3 +19,26 @@ if (!function_exists('flasher')) {
         return $flasher->with($stamps)->addFlash($type, $message, $options);
     }
 }
+
+if (!function_exists('flash')) {
+    /**
+     * @param string $message
+     * @param string $type
+     *
+     * @return \Flasher\Prime\Factory\TemplateFactory
+     */
+    function flash($message = null, $title = null, $type = 'success', array $options = array(), array $stamps = array())
+    {
+        /** @var \Flasher\Prime\Factory\TemplateFactory $flasher */
+        $flasher = app('flasher.template');
+
+        if (null === $message && 0 === func_num_args()) {
+            return $flasher;
+        }
+
+        return $flasher
+            ->title($title)
+            ->with($stamps)
+            ->addFlash($type, $message, $options);
+    }
+}
