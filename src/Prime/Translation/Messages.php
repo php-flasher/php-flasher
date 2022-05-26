7358a3c8b6dfff7f611b20c4f846bfc6c395af18
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-05-26 07:55:37 +0100

    add translation constants

---
+++ src/Prime/Translation/Messages.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the PHPFlasher package.
+ * (c) Younes KHOUBZA <younes.khoubza@gmail.com>
+ */
+
+namespace Flasher\Prime\Translation;
+
+final class Messages
+{
+    const AR = array(
+        'success' => 'نجاح',
+        'error' => 'خطأ',
+        'warning' => 'تحذير',
+        'info' => 'معلومة',
+    );
+
+    const EN = array(
+        'success' => 'Success',
+        'error' => 'Error',
+        'warning' => 'Warning',
+        'info' => 'Info',
+    );
+
+    const FR = array(
+        'success' => 'Succès',
+        'error' => 'Erreur',
+        'warning' => 'Avertissement',
+        'info' => 'Information',
+    );
+}
