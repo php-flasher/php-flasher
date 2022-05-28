0be681699c39dcb65f2542c09f4ba6e870c33fa3
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-05-28 11:50:54 +0100

    add preset support to enable shortcut messages

---
+++ src/Prime/Stamp/PresetStamp.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the PHPFlasher package.
+ * (c) Younes KHOUBZA <younes.khoubza@gmail.com>
+ */
+
+namespace Flasher\Prime\Stamp;
+
+class PresetStamp implements StampInterface
+{
+    /**
+     * @var string
+     */
+    private $preset;
+
+    /**
+     * @param string $preset
+     */
+    public function __construct($preset)
+    {
+        $this->preset = $preset;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPreset()
+    {
+        return $this->preset;
+    }
+}
