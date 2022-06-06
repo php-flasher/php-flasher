74a7f95f11ea1679012bf4e0fd82877ca03f3fca
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-06-06 09:23:11 +0100

    update arabic translations

--- src/Prime/Translation/Messages.php
+++ src/Prime/Translation/Messages.php
@@ -15,7 +15,7 @@ final class Messages
         'warning' => 'تحذير',
         'info' => 'معلومة',
 
-        'The resource was created!' => ':resource تم إنشاؤه',
+        'The resource was created!' => 'تم إنشاء :resource',
         'The resource was updated!' => 'تم تعديل :resource',
         'The resource was saved!' => 'تم حفظ :resource',
         'The resource was deleted!' => 'تم حذف :resource',
