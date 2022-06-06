b36d1523adc4ce11cc3661f8b671a88240db371a
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-06-06 04:23:09 +0100

    add translation parameters to translation stamp class

--- src/Prime/Stamp/TranslationStamp.php
+++ src/Prime/Stamp/TranslationStamp.php
@@ -10,16 +10,35 @@ namespace Flasher\Prime\Stamp;
 final class TranslationStamp implements StampInterface
 {
     /**
+     * @var array<string, mixed>
+     */
+    private $parameters;
+
+    /**
      * @var string|null
      */
     private $locale;
 
     /**
-     * @param string|null $locale
+     * @param array<string, mixed> $parameters
+     * @param string|null          $locale
      */
-    public function __construct($locale = null)
+    public function __construct($parameters = array(), $locale = null)
     {
+        $order = self::parametersOrder($parameters, $locale);
+        $parameters = $order['parameters'];
+        $locale = $order['locale'];
+
+        $this->parameters = $parameters;
         $this->locale = $locale;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
     }
 
     /**
@@ -28,5 +47,26 @@ final class TranslationStamp implements StampInterface
     public function getLocale()
     {
         return $this->locale;
+    }
+
+    /**
+     * @param mixed      $parameters
+     * @param mixed|null $locale
+     *
+     * @return array{parameters: array<string, mixed>, locale: string|null}
+     */
+    public static function parametersOrder($parameters = array(), $locale = null)
+    {
+        if (\is_string($parameters)) {
+            $locale = $parameters;
+            $parameters = array();
+
+            @trigger_error('Since php-flasher/flasher v1.4, passing the locale as first parameter is deprecated and will be removed in v2.0. Use the second parameter instead.', \E_USER_DEPRECATED);
+        }
+
+        return array(
+            'parameters' => $parameters,
+            'locale' => $locale,
+        );
     }
 }
