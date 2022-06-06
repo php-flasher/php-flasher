91a77b42cf1e38ea54f4051d733e5cefb3e2cb0e
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-06-06 04:24:53 +0100

    add translation parameters to TranslatorInterface

--- src/Prime/Translation/TranslatorInterface.php
+++ src/Prime/Translation/TranslatorInterface.php
@@ -10,12 +10,13 @@ namespace Flasher\Prime\Translation;
 interface TranslatorInterface
 {
     /**
-     * @param string      $id
-     * @param string|null $locale
+     * @param string               $id
+     * @param array<string, mixed> $parameters
+     * @param string|null          $locale
      *
      * @return string
      */
-    public function translate($id, $locale = null);
+    public function translate($id, $parameters = array(), $locale = null);
 
     /**
      * @return string
