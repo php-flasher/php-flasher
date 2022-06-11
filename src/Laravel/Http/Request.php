ef8d355077ae16b5275b8c5e3b8b270e4deed527
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-06-11 14:34:22 +0100

    add laravel http request

---
+++ src/Laravel/Http/Request.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the PHPFlasher package.
+ * (c) Younes KHOUBZA <younes.khoubza@gmail.com>
+ */
+
+namespace Flasher\Laravel\Http;
+
+use Flasher\Prime\Http\RequestInterface;
+use Illuminate\Http\Request as LaravelRequest;
+
+final class Request implements RequestInterface
+{
+    /**
+     * @var LaravelRequest
+     */
+    private $request;
+
+    public function __construct(LaravelRequest $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isXmlHttpRequest()
+    {
+        return $this->request->ajax();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasSession()
+    {
+        return $this->request->hasSession();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasType($type)
+    {
+        $session = $this->request->session();
+
+        return $session->has($type);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getType($type)
+    {
+        $session = $this->request->session();
+
+        return $session->get($type);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function forgetType($type)
+    {
+        $session = $this->request->session();
+
+        $session->forget($type);
+    }
+}
