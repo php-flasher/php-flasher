3baa546c8a36ea3f0f9696c586e51e398c93846b
Khoubza Younes <younes.khoubza@smile-maroc.com>
2023-01-21 21:21:37 +0100

    feat: replace maker with composer script
    feat: replace maker with composer script

--- make/composer.mk
+++
@@ -1,9 +0,0 @@
-.PHONY: install update
-
-## 🧙‍ Installs the project dependencies and assets
-install: composer.lock
-	$(COMPOSER) install $(COMPOSER_ARGS)
-
-## Upgrades your dependencies to the latest version according to composer.json, and updates the composer.lock file.
-update:
-	$(COMPOSER) update --lock $(COMPOSER_ARGS)
