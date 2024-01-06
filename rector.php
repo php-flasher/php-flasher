2a739396d455918ad613b8906f2ee8b5717911de
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2024-01-06 20:58:40 +0100

    Wip

--- rector.php
+++ rector.php
@@ -8,7 +8,6 @@ use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__.'/phpstan/',
         __DIR__.'/src/',
         __DIR__.'/tests/',
         __DIR__.'/bin/',
