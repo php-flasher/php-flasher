9dfba426b84493e7e02ca6c5567119d216476770
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2024-01-07 15:56:16 +0100

    Wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,17 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->append([__FILE__])
+;
 
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
-        '@PHPUnit75Migration:risky' => true,
+        'declare_strict_types' => true,
         '@PHP71Migration' => true,
+        '@PHPUnit75Migration:risky' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        'declare_strict_types' => true,
+        'protected_to_private' => false,
+        'native_constant_invocation' => ['strict' => false],
+        'no_superfluous_phpdoc_tags' => [
+            'remove_inheritdoc' => true,
+            'allow_unused_params' => true, // for future-ready params, to be replaced with https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/7377
+        ],
+        'modernize_strpos' => true,
+        'get_class_to_class_keyword' => true,
+        'nullable_type_declaration' => true,
         'trailing_comma_in_multiline' => ['elements' => ['arrays', 'match', 'parameters']],
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+    ->setCacheFile('.php-cs-fixer.cache/cache.json');
