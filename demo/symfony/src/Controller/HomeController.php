f7fd0e0069a2e387d86ad0206e47e6e032708949
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2024-04-12 04:11:58 +0000

    demo: add phpstan in symfony demo with level 9

--- demo/symfony/src/Controller/HomeController.php
+++ demo/symfony/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Flasher\Prime\FlasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,11 +10,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(FlasherInterface $flasher): Response
     {
         $this->addFlash('success', 'welcome from php-flasher');
 
         flash()->info('hello from Home Controller', ['timeout' => 60000]);
+        $flasher->info('hello from Home Controller', ['timeout' => 60000]);
+
+        sweetalert()->success('hello from Home Controller', ['timeout' => 60000]);
+        noty()->success('hello from Home Controller', ['timeout' => 60000]);
+        notyf()->warning('hello from Home Controller', ['timeout' => 60000]);
+
+        toastr()->error('hello from Home Controller', ['timeout' => 60000]);
+        \Flasher\Toastr\Prime\toastr()->error('hello from Home Controller', ['timeout' => 60000]);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
