f61a7d83ce045292157ae5f3e32d56eb56ad8f3f
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2024-05-24 13:33:25 +0100

    test: add tests for FlasherMiddleware and SessionMiddleware registration

--- tests/Laravel/ServiceProviderTest.php
+++ tests/Laravel/ServiceProviderTest.php
@@ -4,40 +4,102 @@ declare(strict_types=1);
 
 namespace Flasher\Tests\Laravel;
 
+use Flasher\Laravel\Middleware\FlasherMiddleware;
+use Flasher\Laravel\Middleware\SessionMiddleware;
 use Flasher\Prime\FlasherInterface;
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\Attributes\DefineEnvironment;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 final class ServiceProviderTest extends TestCase
 {
-    public function testContainerContainServices(): void
+    /**
+     * @param class-string<object> $class
+     */
+    #[DataProvider('servicesDataProvider')]
+    public function testContainerContainServices(string $service, string $class): void
     {
-        $this->assertTrue($this->app->bound('flasher'));
-        $this->assertTrue($this->app->bound('flasher.noty'));
-        $this->assertTrue($this->app->bound('flasher.notyf'));
-        $this->assertTrue($this->app->bound('flasher.sweetalert'));
-        $this->assertTrue($this->app->bound('flasher.toastr'));
-
-        $this->assertInstanceOf(\Flasher\Prime\Flasher::class, $this->app->make('flasher'));
-        $this->assertInstanceOf(\Flasher\Noty\Prime\Noty::class, $this->app->make('flasher.noty'));
-        $this->assertInstanceOf(\Flasher\Notyf\Prime\Notyf::class, $this->app->make('flasher.notyf'));
-        $this->assertInstanceOf(\Flasher\SweetAlert\Prime\SweetAlert::class, $this->app->make('flasher.sweetalert'));
-        $this->assertInstanceOf(\Flasher\Toastr\Prime\Toastr::class, $this->app->make('flasher.toastr'));
+        $this->assertTrue($this->app->bound($service));
+        $this->assertInstanceOf($class, $this->app->make($service));
     }
 
-    public function testFlasherCanCreateServicesFromAlias(): void
+    /**
+     * @param class-string<object> $expectedClass
+     */
+    #[DataProvider('flasherAdapterProvider')]
+    public function testFlasherCanCreateServicesFromAlias(string $alias, string $expectedClass): void
     {
         /** @var FlasherInterface $flasher */
         $flasher = $this->app->make('flasher');
 
-        $adapter = $flasher->use('noty');
-        $this->assertInstanceOf(\Flasher\Noty\Prime\Noty::class, $adapter);
+        $adapter = $flasher->use($alias);
+        $this->assertInstanceOf($expectedClass, $adapter);
+    }
 
-        $adapter = $flasher->use('notyf');
-        $this->assertInstanceOf(\Flasher\Notyf\Prime\Notyf::class, $adapter);
+    public function testMiddlewareIsRegistered(): void
+    {
+        $this->assertTrue($this->hasMiddleware(FlasherMiddleware::class));
+        $this->assertTrue($this->hasMiddleware(SessionMiddleware::class));
+    }
 
-        $adapter = $flasher->use('sweetalert');
-        $this->assertInstanceOf(\Flasher\SweetAlert\Prime\SweetAlert::class, $adapter);
+    #[DefineEnvironment('disableInjectAssets')]
+    public function testFlasherMiddlewareDisabled(): void
+    {
+        $this->assertFalse($this->app->bound(FlasherMiddleware::class));
+        $this->assertFalse($this->hasMiddleware(FlasherMiddleware::class));
+    }
 
-        $adapter = $flasher->use('toastr');
-        $this->assertInstanceOf(\Flasher\Toastr\Prime\Toastr::class, $adapter);
+    #[DefineEnvironment('disableFlashBag')]
+    public function testSessionMiddlewareDisabled(): void
+    {
+        $this->assertFalse($this->app->bound(SessionMiddleware::class));
+        $this->assertFalse($this->hasMiddleware(SessionMiddleware::class));
+    }
+
+    private function hasMiddleware(string $middleware): bool
+    {
+        $kernel = $this->app->make(\Illuminate\Foundation\Http\Kernel::class);
+        $middlewares = $kernel->getMiddlewareGroups()['web'];
+
+        return \in_array($middleware, $middlewares);
+    }
+
+    /**
+     * @return array<array<string>>
+     */
+    public static function servicesDataProvider(): array
+    {
+        return [
+            ['flasher', \Flasher\Prime\Flasher::class],
+            ['flasher.noty', \Flasher\Noty\Prime\Noty::class],
+            ['flasher.notyf', \Flasher\Notyf\Prime\Notyf::class],
+            ['flasher.sweetalert', \Flasher\SweetAlert\Prime\SweetAlert::class],
+            ['flasher.toastr', \Flasher\Toastr\Prime\Toastr::class],
+            [FlasherMiddleware::class, FlasherMiddleware::class],
+            [SessionMiddleware::class, SessionMiddleware::class],
+        ];
+    }
+
+    /**
+     * @return array<array<string>>
+     */
+    public static function flasherAdapterProvider(): array
+    {
+        return [
+            ['noty', \Flasher\Noty\Prime\Noty::class],
+            ['notyf', \Flasher\Notyf\Prime\Notyf::class],
+            ['sweetalert', \Flasher\SweetAlert\Prime\SweetAlert::class],
+            ['toastr', \Flasher\Toastr\Prime\Toastr::class],
+        ];
+    }
+
+    protected function disableInjectAssets(Application $app): void
+    {
+        $app['config']->set('flasher.inject_assets', false);
+    }
+
+    protected function disableFlashBag(Application $app): void
+    {
+        $app['config']->set('flasher.flash_bag', false);
     }
 }
