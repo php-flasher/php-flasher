e1a6a85c763bf40a4be9f74a68809309da0ebf0c
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-03-08 11:40:49 +0000

    add facebook theme

---
+++ src/Prime/Resources/assets/themes/facebook/index.ts
@@ -0,0 +1,27 @@
+/**
+ * @file PHPFlasher Facebook Theme Registration
+ * @description Registers the Facebook theme with PHPFlasher
+ * @author yoeunes
+ */
+import flasher from '../../index'
+import { facebookTheme } from './facebook'
+
+/**
+ * Register the Facebook theme.
+ *
+ * This theme provides notifications styled after Facebook's interface design.
+ * The registration makes the theme available under the name 'facebook'.
+ *
+ * The Facebook theme can be used by calling:
+ * ```typescript
+ * flasher.use('theme.facebook').success('Your post was published successfully');
+ * ```
+ *
+ * Key features:
+ * - Facebook-like notification cards with rounded corners and subtle shadows
+ * - Type-specific circular icons with branded colors
+ * - Timestamp display similar to Facebook's time format
+ * - Interactive elements with hover states
+ * - Facebook's distinctive font family and color palette
+ */
+flasher.addTheme('facebook', facebookTheme)
