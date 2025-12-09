91ab4fa0f40a41fd7a6a0547a7abde08ccbca630
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-09 11:23:28 +0100

    Updates Flasher dependencies to version 2.3.0
    Updates all flasher dependencies in composer.json files and the main Flasher version constant to the new version 2.3.0.
    
    This ensures that all packages are using the latest compatible version of Flasher.

--- src/Prime/Flasher.php
+++ src/Prime/Flasher.php
@@ -29,7 +29,7 @@ final readonly class Flasher implements FlasherInterface
     /**
      * Current version of PHPFlasher.
      */
-    public const VERSION = '2.2.2';
+    public const VERSION = '2.3.0';
 
     /**
      * Creates a new Flasher instance.
