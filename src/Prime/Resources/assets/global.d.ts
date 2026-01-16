521746f8132f8496b50914fa7bb8afade4b5490b
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2026-01-16 01:28:29 +0100

    Simplify remaining comments in global.d.ts and SCSS theme files
    - Remove file headers and verbose documentation from 2 global.d.ts files
    - Simplify 12 SCSS theme files by removing multi-line documentation blocks
    - Remove verbose inline comments that just repeat what the code does
    - Keep helpful inline comments for non-obvious code
    
    Files modified:
    - src/Prime/Resources/assets/global.d.ts
    - src/Toastr/Prime/Resources/assets/global.d.ts
    - src/Prime/Resources/assets/themes/aurora/aurora.scss
    - src/Prime/Resources/assets/themes/facebook/facebook.scss
    - src/Prime/Resources/assets/themes/google/google.scss
    - src/Prime/Resources/assets/themes/ios/ios.scss
    - src/Prime/Resources/assets/themes/jade/jade.scss
    - src/Prime/Resources/assets/themes/material/material.scss
    - src/Prime/Resources/assets/themes/minimal/minimal.scss
    - src/Prime/Resources/assets/themes/neon/neon.scss
    - src/Prime/Resources/assets/themes/onyx/onyx.scss
    - src/Prime/Resources/assets/themes/ruby/ruby.scss
    - src/Prime/Resources/assets/themes/sapphire/sapphire.scss
    - src/Prime/Resources/assets/themes/slack/slack.scss

--- src/Prime/Resources/assets/global.d.ts
+++ src/Prime/Resources/assets/global.d.ts
@@ -1,23 +1,7 @@
-/**
- * @file TypeScript Global Declarations
- * @description Type definitions for global objects
- * @author Younes ENNAJI
- */
 import type Flasher from './flasher'
 
-/**
- * Extend the Window interface to include the global flasher instance.
- *
- * This allows TypeScript to recognize window.flasher as a valid property
- * with proper type information.
- */
 declare global {
     interface Window {
-        /**
-         * Global PHPFlasher instance.
-         *
-         * Available as window.flasher in browser environments.
-         */
         flasher: Flasher
     }
 }
--- src/Toastr/Prime/Resources/assets/global.d.ts
+++ src/Toastr/Prime/Resources/assets/global.d.ts
@@ -1,10 +1,3 @@
-/**
- * @file TypeScript Global Declarations for Toastr
- * @description Type definitions for jQuery and Toastr globals
- * @author Younes ENNAJI
- */
-
-// Declare jQuery on the global window object
 interface Window {
     jQuery?: any
     $?: any
