06f75b21eb96b5956546848ac74cb3bda8c39f5f
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2026-01-25 06:03:39 +0100

    Fix jQuery dependency for Toastr library
    Import jQuery and make it globally available before Toastr is loaded.
    This prevents console errors about jQuery not being found.

--- docs/assets/js/controllers/toastr_controller.js
+++ docs/assets/js/controllers/toastr_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from '@hotwired/stimulus'
 import { showNotificationsForHandler } from '../show_notifications'
 
+import jQuery from 'jquery'
 import 'toastr/build/toastr.min.css'
+
+// Make jQuery available globally for toastr
+window.jQuery = jQuery
+window.$ = jQuery
 
 export default class extends Controller {
     connect() {
