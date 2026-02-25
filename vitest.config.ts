ea0dccc961a23cb89e83ea3ccd9c9f22e6ebe63b
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2026-02-25 15:58:13 +0000

    add test coverage for PHPUnit and Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
+            reportsDirectory: 'coverage/vitest',
             include: ['src/*/Resources/assets/**/*.ts'],
             exclude: [
                 '**/index.ts',
