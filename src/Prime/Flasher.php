bb344a7cb8729c1e2539643fb2f24bae05abf48e
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2026-08-30 21:04:39 +0100

    Update version to 2.6.2

--- src/Prime/Flasher.php
+++ src/Prime/Flasher.php
@@ -18,7 +18,7 @@ final readonly class Flasher implements FlasherInterface
 {
     use ForwardsCalls;
 
-    public const VERSION = '2.6.1';
+    public const VERSION = '2.6.2';
 
     public function __construct(
         private string $default,
